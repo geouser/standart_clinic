d46501974982317bbed95297fb4f2213a2bddf7a
rusyi <olehrusy@gmail.com>
2017-03-24 16:47:15 +0200

    ds

--- js/main.js
+++ js/main.js
@@ -41,48 +41,13 @@ jQuery(document).ready(function($) {
     });
     
     /*---------------------------
-                                PAGE ANCHORS
-    ---------------------------*/
-    $('.mainNav a, .anchor').click(function() {
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top - 50
-        }, 800);
-        return false;
-    });
-
-    /*---------------------------
-                                ACTIVATE MENU ITEM OVER CURRENT SECTION
-    ---------------------------*/
-    var $sections = $('section');
-    $(window).scroll(function(){
-        var currentScroll = $(this).scrollTop();
-        var $currentSection;
-        var windowHalf = $(window).height() / 2;
-        
-        $sections.each(function(){
-          var divPosition = $(this).offset().top - windowHalf;
-          
-          if( divPosition - 1 < currentScroll ){
-            $currentSection = $(this);
-          }
-        var id = $currentSection.attr('id');
-          $('a').removeClass('active');
-          $("[href=#"+id+"]").addClass('active');
-        })
-    });
-
-    /*---------------------------
                                   MENU TOGGLE
     ---------------------------*/
     $('.menu-button').on('click', function(event) {
         event.preventDefault();
         $(this).toggleClass('active');
-        $(this).siblings('header').toggleClass('active');
-        if ($('header').hasClass('active')) {
-                $('body').css('overflow', 'hidden');
-            } else {
-                $('body').css('overflow', 'visible');
-            }
+        $(this).siblings('.mainNav').toggleClass('active');
+        $(this).parent().parent().toggleClass('active');
     });
 
 
